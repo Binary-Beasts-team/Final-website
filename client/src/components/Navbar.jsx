3e3c5e8ad191360a405da36756b68aa517567f42
chaitnya23 <chaitnyagiri2306@gmail.com>
2022-06-30 12:58:30 +0530

    add responsiveness

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { GrSearch } from 'react-icons/gr'
+
+
 
 export default function Navbar() {
 
@@ -9,15 +12,15 @@ export default function Navbar() {
     return (
 
         <>
-            <nav className='py-1 md:flex justify-between bg-red-600  text-white'>
+            <nav className='py-2 lg:text-base text-2xl md:flex justify-between bg-red-600  text-white lg:h-14 h-[140px]'>
                 <div className="logo">Logo</div>
 
-                <div className="search-box-1 w-[25%] px-1 bg-white rounded-sm">
-                    <input type="text" placeholder='search for your food ' className='w-80% p-1 px-4' />
-                    <i data-feather="search"></i>
+                <div className="search-box-1 lg:w-[25%] w-[60%] bg-white rounded-sm flex justify-between gap-2  lg:h-[90%] h-[60%] my-auto">
+                    <input type="text" placeholder='search for your food ' className='w-90% text-black outline-none p-1 px-3' />
+                    <GrSearch size={30} style={{margin:"auto"}}/>
                 </div>
                 <div className=' lg:block hidden'>
-                    <ul className="flex gap-6 items-center">
+                    <ul className="flex gap-6 items-center font-semibold">
                         <li className='nav-link px-2 rounded-sm'>Home</li>
                         <li className='nav-link px-2 rounded-sm'>About</li>
                         <li className='nav-link px-2 rounded-sm'>My orders</li>
@@ -25,16 +28,16 @@ export default function Navbar() {
 
                     </ul>
                 </div>
-                <div className='lg:hidden  block mr-4'>
-                    <GiHamburgerMenu size={25} style={{ margin: "auto" }} onClick={()=>setdisplayMenu(!displayMenu)} />
+                <div className='lg:hidden  block mr-4 my-auto'>
+                    <GiHamburgerMenu size={70} style={{ margin: "auto" }} onClick={()=>setdisplayMenu(!displayMenu)} />
                 </div>
             </nav>
-            <div className={`mobile-menu  text-white text-center font-semibold ${displayMenu?"block":"hidden"} `}>
+            <div className={`mobile-menu sm:text-3xl  text-white text-center font-semibold ${displayMenu?"block":"hidden"} `}>
                 <ul className='flex-col gap-7  bg-red-500 p-3'>
-                    <li className='py-1'>Profile</li>
-                    <li  className='py-1'>Home</li>
-                    <li className='py-1'>About</li>
-                    <li className='py-1'>My orders</li>
+                    <li className='py-1 my-2'>Profile</li>
+                    <li  className='py-1 my-6'>Home</li>
+                    <li className='py-1 my-6'>About</li>
+                    <li className='py-1 my-6'>My orders</li>
                 </ul>
             </div>
 
