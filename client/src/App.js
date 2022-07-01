37dc8889fbc150371b035585e2e23a3e3e6770f3
Keshav <keshavjha018@gmail.com>
2022-07-01 23:25:14 +0530

    resolved conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from "./pages/Home";
+import Outpass from "./pages/Outpass";
+import Profile from "./pages/Profile";
 import React from 'react';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Profile from "./pages/Profile";
 
 
 function App() {
@@ -15,9 +16,11 @@ function App() {
     <ToastContainer position="top-center" autoClose="2500"/>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home/>} />
+        <Route exact path="/" element={<Home/>} />
+
           <Route exact path="/user/signup" element={<Signup />} />
           <Route exact path="/user/login" element={<Login />} />
+          <Route exact path="/user/outpass" element={<Outpass />} />
           <Route exact path="/profile" element={<Profile />} />
         </Routes>
       </Router>
