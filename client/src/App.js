884a6517ad6d625dde99b02b41a500b661ae70a4
aditya-jadhav-18 <89136092+aditya-jadhav-18@users.noreply.github.com>
2022-07-03 11:21:14 +0530

    outpass/added

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,9 @@ import Home from "./pages/Home";
 import Outpass from "./pages/Outpass";
 import Profile from "./pages/Profile"
 import React from 'react';
-import Verify from "./pages/verify";
-import Resetpassword from "./pages/resetpassword";
-
+import OutpassForm from './pages/OutpassForm'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Navbar from "./components/Navbar";
-import OutpassForm from "./pages/OutpassForm";
-import Footer from "./components/Footer"
 
 
 function App() {
@@ -20,20 +15,17 @@ function App() {
     <>
     <ToastContainer position="top-center" autoClose="2500"/>
       <Router>
-        <Navbar/>
-
         <Routes>
-          <Route exact path="/" element={<Home/>} />
+
+        <Route exact path="/" element={<Home/>} />
           <Route exact path="/user/signup" element={<Signup />} />
           <Route exact path="/user/login" element={<Login />} />
           <Route exact path="/user/outpass" element={<Outpass />} />
-          <Route exact path="/:id" element={<Profile />} />
-          <Route exact path="/verify/mail" element={<Verify />} />
-          <Route exact path="/verify/resetpassword" element={<Resetpassword />} />
-          <Route exact path="/apply" element={<OutpassForm />} />
+          <Route exact path="/outpass/form" element={<OutpassForm />} />
+          <Route exact path="/profile" element={<Profile />} />
+  
         </Routes>
-
-        <Footer />
+        
       </Router>
     </>
   );
