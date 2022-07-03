37b68549cc65b6ccba1fdf9256b6895f641e4257
Keshav <keshavjha018@gmail.com>
2022-07-03 13:39:42 +0530

    css + home modified

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import React from 'react';
 import OutpassForm from './pages/OutpassForm'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -15,16 +17,19 @@ function App() {
     <>
     <ToastContainer position="top-center" autoClose="2500"/>
       <Router>
-        <Routes>
 
-        <Route exact path="/" element={<Home/>} />
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
           <Route exact path="/user/signup" element={<Signup />} />
           <Route exact path="/user/login" element={<Login />} />
           <Route exact path="/user/outpass" element={<Outpass />} />
           <Route exact path="/outpass/form" element={<OutpassForm />} />
           <Route exact path="/profile" element={<Profile />} />
-  
         </Routes>
+
+        <Footer/>
         
       </Router>
     </>
