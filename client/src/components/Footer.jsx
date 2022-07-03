4dcd9168853cab56a480064e51e1472ec6d41aa1
Keshav <keshavjha018@gmail.com>
2022-07-03 09:50:47 +0530

    footer done

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Footer.css"
+import {AiFillGithub} from "react-icons/ai"
+import {BsLinkedin, BsTwitter} from "react-icons/bs"
+
+function Footer() {
+  return (
+    <>
+      <footer className='footer'>
+        <div className="ele-row socialIcons">
+          <a href="https://github.com/Binary-Beasts-team" target="_blank" rel="noreferrer noopener"><AiFillGithub className='IconItem'/></a>
+          <a href="#" target="_blank" rel="noreferrer noopener"><BsLinkedin className='IconItem'/></a>
+          <a href="#" target="_blank" rel="noreferrer noopener"><BsTwitter className='IconItem'/></a>
+        </div>
+        <div class="ele-row">
+          Copyright © 2022 - All rights reserved || Designed and Developed By: Binary Beasts 
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
